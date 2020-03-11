2bc3e0556363eced2687795c03e06d237dc80514
otaki444 <xm.ms.430x@gmail.com>
2020-03-11 17:24:25 +0900

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '27c8ec87a1bdd2aa5cfe1c1c6b87a7e1ffccd6e5b6e9ac0f9eb5b20a107d498376121379bd756f3ac1c2bd1631f40ba0037d5d40a9be7e44faec01121a83642c'
